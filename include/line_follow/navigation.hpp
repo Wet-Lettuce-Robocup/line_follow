8c158de002e5e9e07b22d4efe0c64059f9698922
William D'Olier <william@dolier.net>
2026-05-13 08:44:39 +1000

    new files for other nodes

---
+++ include/line_follow/navigation.hpp
@@ -0,0 +1,15 @@
+#include <rclcpp_lifecycle/lifecycle_node.hpp>
+
+class NavigationNode : rclcpp_lifecycle::LifecycleNode {
+public:
+  NavigationNode();
+
+  CallbackReturn on_configure(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_activate(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_cleanup(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_shutdown(const rclcpp_lifecycle::State &) override;
+
+private:
+
+};
